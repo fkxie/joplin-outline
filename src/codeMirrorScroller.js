d18bbfa43d9ea93c531af1259152df806d43e342
fkxie <13857501867@sina.cn>
2024-05-19 17:28:23 +0800

    update comments

--- src/codeMirrorScroller.js
+++ src/codeMirrorScroller.js
@@ -1,8 +1,16 @@
 function plugin(CodeMirror) {
+  // 注册codeMirror的scrollToLine方法
   CodeMirror.defineExtension('scrollToLine', function scrollToLine(lineno) {
     // temporary fix: sometimes the first coordinate is incorrect,
     // resulting in a difference about +- 10 px,
     // call the scroll function twice fixes the problem.
+
+    // 第一个参数为水平滚动的位置，第二个参数为垂直滚动的位置
+    // this.charCoords方法获取指定位置的字符的坐标。
+    // 这里传入的对象{ line: lineno, ch: 0 }表示要获取的位置是lineno行的第一个字符。
+    // 'local'参数表示获取相对于滚动容器的坐标。
+    // .top 表示该行顶部的垂直位置
+    // 滚动两次为了到正确位置
     this.scrollTo(null, this.charCoords({ line: lineno, ch: 0 }, 'local').top);
     this.scrollTo(null, this.charCoords({ line: lineno, ch: 0 }, 'local').top);
   });
--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,18 @@ import panelHtml from './panelHtml';
 
 joplin.plugins.register({
   async onStart() {
+
+    // 注册settings(各项属性)
     await registerSettings();
 
+    // 注册CodeMirror插件(跳转行)
     await joplin.contentScripts.register(
       ContentScriptType.CodeMirrorPlugin,
       'codeMirrorScroller',
       './codeMirrorScroller.js',
     );
 
+    // 等效于 const panels = joplin.views.panels;
     const { panels } = joplin.views;
     const view = await (panels as any).create('outline.panel');
 
@@ -24,18 +28,21 @@ joplin.plugins.register({
     await panels.addScript(view, './katex/katex.min.css');
     await panels.addScript(view, './katex/katex.min.js');
 
+    // Called when a message is sent from the webview (using postMessage).
     await panels.onMessage(view, async (message: any) => {
       if (message.name === 'scrollToHeader') {
         const editorCodeView = await joplin.settings.globalValue('editor.codeView');
         const noteVisiblePanes = await joplin.settings.globalValue('noteVisiblePanes');
         if (editorCodeView && noteVisiblePanes.includes('editor')) {
           // scroll in raw markdown editor
+          // 通过editor.execCommand执行CodeMirror的scrollToLine方法
           await joplin.commands.execute('editor.execCommand', {
             name: 'scrollToLine',
             args: [message.lineno],
           });
         } else {
           // scroll in WYSIWYG editor or viewer
+          // scrollToHash是Joplin的内置命令
           await joplin.commands.execute('scrollToHash', message.hash);
         }
       } else if (message.name === 'contextMenu') {
@@ -45,14 +52,21 @@ joplin.plugins.register({
         if (message.hash === '') {
           innerLink = `[${noteTitle}](:/${noteId})`;
         } else {
+          // 跳到id为noteId的note的hash为message.hash的位置
           innerLink = `[${noteTitle}#${message.content}](:/${noteId}#${message.hash})`;
         }
-
+        
+        // 创建名为input的Element
         const input = document.createElement('input');
+        // 设置input的value属性
         input.setAttribute('value', innerLink);
+        // 将input添加到body中，只有当元素被插入到文档中时，才能对其执行select()和copy操作。
         document.body.appendChild(input);
+        // 选中input的内容
         input.select();
+        // 复制input的内容
         document.execCommand('copy');
+        // 移除input
         document.body.removeChild(input);
       }
     });
@@ -61,6 +75,7 @@ joplin.plugins.register({
       const note = await joplin.workspace.selectedNote();
 
       // Settings
+      // Gets a setting value (only applies to setting you registered from your plugin)
       const autoHide = await settingValue('autoHide');
 
       let headers;
